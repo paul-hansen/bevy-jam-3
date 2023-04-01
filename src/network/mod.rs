53694888ce2ba9cb35a5c259930d8e76c63abac0
Paul <mail@paul.rs>
2023-04-01 14:20:10 -0500

    Client connection working

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -3,16 +3,25 @@ mod util;
 use std::net::IpAddr;
 
 use bevy::prelude::*;
+use bevy_replicon::renet::ServerEvent;
+use bevy_replicon::ReplicationPlugins;
 
 #[derive(Resource, Eq, PartialEq, PartialOrd, Ord)]
-struct NetworkInfo{
-  pub public_ip: IpAddr,
+struct NetworkInfo {
+    pub public_ip: IpAddr,
 }
 
 pub struct NetworkPlugin;
 
-impl Plugin for NetworkPlugin{
+impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut App) {
-        todo!()
+        app.add_plugins(ReplicationPlugins);
+        app.add_system(log_network_events);
+    }
+}
+
+fn log_network_events(mut events: EventReader<ServerEvent>) {
+    for event in events.iter() {
+        info!("{event:?}");
     }
 }
