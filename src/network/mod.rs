cbc54246ac35ef6b0fba5c99069e1034765cae25
Cayle Bray <cayle.bray@braymatter.com>
2023-04-01 15:05:11 -0500

    feature: Public IP Autodiscovery

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,18 +1,63 @@
 mod util;
 
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::{IoTaskPool, TaskPool}};
 
-#[derive(Resource, Eq, PartialEq, PartialOrd, Ord)]
-struct NetworkInfo{
-  pub public_ip: IpAddr,
+#[derive(Resource)]
+pub struct NetworkInfo {
+    pub public_ip: Option<IpAddr>,
+    task_pool: &'static IoTaskPool,
 }
 
+impl Default for NetworkInfo{
+    fn default() -> Self {
+        let mut val = Self { public_ip: Default::default(), task_pool: IoTaskPool::init(||{TaskPool::new()}) };
+        
+        val.fetch_ip();
+        
+        val
+    }
+}
+impl NetworkInfo {
+    pub fn fetch_ip(&mut self) {
+        self.task_pool.scope(|scope| {
+            scope.spawn(async {
+              match surf::get("https://api.ipify.org/").await{
+                Ok(mut response) => {
+                  match response.body_string().await{
+                    Ok(ip_text) => {
+                      match ip_text.parse::<Ipv4Addr>() {
+                        Ok(address) => {
+                          info!("Found Public IP: {}", address);
+                          self.public_ip = Some(IpAddr::V4(address));
+                        }, 
+                        Err(e) => {
+                          self.public_ip = None;
+                          warn!("Could not parse ip [{}]: {}", ip_text, e)
+                        }
+                      }
+                    },
+                    Err(e) => {
+                      self.public_ip = None;
+                      warn!("Could not marshal response body to string: {}", e)
+                    }
+                  }
+
+                }, 
+                Err(e) => {
+                  self.public_ip = None;
+                  warn!("Could not get get ip: {}", e);
+                }
+              }
+            });
+        });
+    }
+}
 pub struct NetworkPlugin;
 
-impl Plugin for NetworkPlugin{
+impl Plugin for NetworkPlugin {
     fn build(&self, app: &mut App) {
-        todo!()
+        app.init_resource::<NetworkInfo>();
     }
 }
